34d7fbc75a0988fca71258a1cfb96d26e5c13bcc
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-03 20:44:51 -0600

    juego.py

---
+++ juego.py
@@ -0,0 +1,7 @@
+#Importacion de libreria de ursina y controlador
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+#Verificacion antes de continuar para verificar que funciona ursina.
+app run()
