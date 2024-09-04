b3fa215eb1c11261af7d0dba7133ce9832001fd3
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-03 20:57:00 -0600

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,18 +1,19 @@
-#Importacion de libreria de ursina y controlador
+# Importación de librería de ursina y controlador
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
-class Voxel(Button)
-    def __init__(self.position=(0,0,0));
-        super() __init__(
-            parent = scene,
-          position = position,
-          model = 'cube',
-          origin_y = '.5,
-          texture = 'grass',
-          color = color rgb(255,255,255)
-          highlight_color = color.lime.
+class Voxel(Button):
+    def __init__(self, position=(0, 0, 0)):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model='cube',
+            origin_y=0.5,
+            texture='grass',
+            color=color.rgb(255, 255, 255),
+            highlight_color=color.lime
         )
-app run()
+
+app.run()
